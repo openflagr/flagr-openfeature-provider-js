6e751720a1e9f63d39db1b68e4ba59d54a05d448
Brandyn Britton <brandynbb96@gmail.com>
2026-02-06 17:31:27 +0800

    feat: implementation of flagrBatchEvaluator

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export { FlagrProvider } from './FlagrProvider';
 export type { FlagrProviderConfig } from './types/FlagrProviderConfig';
 export type { FlagrEvaluationRequest } from './types/flagr/evaluation/FlagrEvaluationRequest';
 export type { FlagrEvaluationResponse } from './types/flagr/evaluation/FlagrEvaluationResponse';
+export type {
+  FlagrBatchEvaluationRequest,
+  FlagrBatchEntity,
+} from './types/flagr/evaluation/FlagrBatchEvaluationRequest';
+export type { FlagrBatchEvaluationResponse } from './types/flagr/evaluation/FlagrBatchEvaluationResponse';
